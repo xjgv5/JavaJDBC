2df6f26c64c022ab9ae3b42ec5ece8f8bc48445f
Javier Gomez <javier.gov@outlook.es>
2023-12-02 22:22:43 -0600

    Conexion con la bd usando el patron Singleton

--- src/main/java/com/xjgv/javaJDBC/EjemploJDBC.java
+++ src/main/java/com/xjgv/javaJDBC/EjemploJDBC.java
@@ -1,14 +1,13 @@
 package com.xjgv.javaJDBC;
 
+import com.xjgv.javaJDBC.util.ConexionBaseDatos;
+
 import java.sql.*;
 
 public class EjemploJDBC {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/java_curso?serverTimezone=Mexico/General";
-        String username = "root";
-        String password = "admin";
 
-        try(Connection conection = DriverManager.getConnection(url, username, password);
+        try(Connection conection = ConexionBaseDatos.getInstance();
         Statement statement = conection.createStatement();
         ResultSet resultado = statement.executeQuery("SELECT * FROM productos")) {
 
