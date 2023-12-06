c0a3b5dd5d91320c0d21101e34fa1fd16d6efdfd
Javier Gomez <javier.gov@outlook.es>
2023-12-05 22:27:02 -0600

    Conexion con la bd usando DAO

--- src/main/java/com/xjgv/javaJDBC/EjemploJDBC.java
+++ src/main/java/com/xjgv/javaJDBC/EjemploJDBC.java
@@ -6,16 +6,31 @@ import com.xjgv.javaJDBC.repositorio.Repositorio;
 import com.xjgv.javaJDBC.util.ConexionBaseDatos;
 
 import java.sql.*;
+import java.util.Date;
 
 public class EjemploJDBC {
     public static void main(String[] args) {
 
         try(Connection conection = ConexionBaseDatos.getInstance()) {
             Repositorio<Producto> repositorio = new ProductoRepositorioImpl();
+            System.out.println("********************** Listar **********************");
             repositorio.listar().forEach(System.out::println);
 
+
+            System.out.println("********************** Obtener por ID **********************");
             System.out.println(repositorio.porId(1L));
 
+            System.out.println("********************** Obtener por ID **********************");
+            Producto producto = new Producto();
+            producto.setNombre("Teclado mecanico");
+            producto.setPrecio(500);
+            producto.setFechaRegistro(new Date());
+
+            repositorio.guardar(producto);
+            System.out.println("Producto guardado con exito");
+
+            repositorio.listar().forEach(System.out::println);
+            
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
